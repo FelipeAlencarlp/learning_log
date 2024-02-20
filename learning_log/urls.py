1b1f8b0c79d322f9506f7e189cf13b44b45d61c3
Felipe <felipealencar.0014@gmail.com>
2024-02-20 14:08:27 -0300

    Projeto finalizado

---
+++ learning_log/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('learning_logs.urls')),
+    path('users/', include('users.urls')),
+]
